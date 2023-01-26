fdf90891039363771234602324aec6ff6d4f0808
so1454 <122778253+so1454@users.noreply.github.com>
2023-01-26 09:15:04 +0900

    230126

---
+++ Ex13MVC/src/ex13mvc/Main.java
@@ -0,0 +1,23 @@
+package ex13mvc;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//디자인 패턴 : 계속해서 같은 문제가 발생하여, 이 문제를 해결하고자
+		// 형식, 규칙을 지정해 놓은 것
+		// MVC 패턴
+		// Model : 데이터를 저장하거나, 조작하기 위한 코드들의 모음
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
