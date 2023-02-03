46a6d58f3d330fd7483575b20229f1e153465547
so1454 <122778253+so1454@users.noreply.github.com>
2023-02-03 09:05:13 +0900

    0223

---
+++ XXXminifinal_tryresult/src/test/Menu.java
@@ -0,0 +1,32 @@
+package test;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//	
+//	System.out.printf("           %n");
+//	System.out.printf("           %n");
+//	System.out.printf("           %n");
+//	System.out.printf("           %n");
+//	System.out.printf("           %n");
+//	System.out.printf("           %n");
+//	System.out.printf("           %n");
+//	System.out.printf("           %n");
+//	System.out.printf("           %n");
+		   System.out.printf("     ____        __                 __                       __          %n");
+		   System.out.printf("     /\  _`\     /\ \__             /\ \                     /\ \__       %n");
+		   System.out.printf("     \ \,\L\_\   \ \ ,_\   __  __   \_\ \      __     ___    \ \ ,_\      %n");
+		   System.out.printf("      \/_\__ \    \ \ \/  /\ \/\ \  /'_` \   /'__`\ /' _ `\   \ \ \/      %n");
+		   System.out.printf("        /\ \L\ \   \ \ \_ \ \ \_\ \/\ \L\ \ /\  __/ /\ \/\ \   \ \ \_     %n");
+		   System.out.printf("        \ `\____\   \ \__\ \ \____/\ \___,_\\ \____\\ \_\ \_\   \ \__\    %n");
+		   System.out.printf("         \/_____/    \/__/  \/___/  \/__,_ / \/____/ \/_/\/_/    \/__/    %n");
+	
+	
+	
+	
+	
+	}
+
+}
